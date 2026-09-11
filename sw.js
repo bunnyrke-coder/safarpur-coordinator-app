5b58cad5ca9bd51e77c8419d1e4a7bdcc80ebe54
bunnyrke-coder <bunnyrke@gmail.com>
2026-09-11 16:49:59 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE="safarpur-coordinator-v1";
+const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
+      const copy=x.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return x;
+    }).catch(()=>caches.match("./index.html"))));
+  }
+});
